bbeaacc71fa022874b3e46057a8a7cae82501dc2
Abhishek More <95474018+Abhi-More@users.noreply.github.com>
2022-09-21 10:55:38 +0530

    Create Pattern1.java

---
+++ *Patterns/Pattern1.java
@@ -0,0 +1,26 @@
+/*
+for n = 6
+
+*     
+**    
+***   
+****  
+***** 
+******
+*/
+import java.util.*;
+public class Pattern1{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
